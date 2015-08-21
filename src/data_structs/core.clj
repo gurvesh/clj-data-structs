31f753ce922895f9ec4b554c7c62a677d89887df
Gurvesh <guru01@gmail.com>
2015-08-21 16:06:31 +0100

    rec1

--- src/data_structs/core.clj
+++ src/data_structs/core.clj
@@ -44,3 +44,46 @@
                    (all-subvecs-from-beginning remaining))
              (into [] (rest remaining)))
       result)))
+
+(defn longest-match-w-beginning
+  [left-array right-array]
+  (let [all-left-chunks (all-subvecs left-array)
+        all-right-chunks-from-beginning (all-subvecs-from-beginning right-array)
+        all-matches (cset/intersection all-right-chunks-from-beginning
+                                       all-left-chunks)]
+    (->> all-matches
+         (sort-by count >)
+         first)))
+
+(defn pos-of-subvec
+  [sv v]
+  {:pre [(<= (count sv)
+             (count v))]}
+  (loop [cursor 0]
+    (if (or (empty? v)
+            (empty? sv)
+            (= cursor (count v)))
+      nil
+      (if (= (subvec v cursor
+                     (+ (count sv)
+                        cursor))
+             sv)
+        cursor
+        (recur (inc cursor))))))
+
+(defn LZ77-STEP
+  [window look-ahead]
+  (let [longest (longest-match-w-beginning window look-ahead)]
+    (if-let [pos-subv-w (pos-of-subvec longest window)]
+      (let [distance (- (count window) pos-subv-w)
+            pos-subv-l (pos-of-subvec longest look-ahead)
+            the-char (first (subvec look-ahead
+                                    (+ pos-subv-l
+                                       (count longest))))]
+        {:distance distance
+         :length (count longest)
+         :char the-char})
+      {:distance 0
+       :length 0
+       :char (first look-ahead)})))
+
